ab3958e90fb3d54361aceea0fcb655929e1a5e91
dbuchanan25 <40598513+dbuchanan25@users.noreply.github.com>
2018-12-04 07:16:18 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<?php
+    echo'
+        <link rel="stylesheet" href="style.css" type="text/css">
+        ';
+    echo'
+    <table align="center" width = 100%>
+        <tr>
+            <td align = left width = 50% style="border:none;">
+            <img src="ProvidenceSmall.png" alt="" border=3 height=158 width=657></img>
+            </td>
+            <td align = right width = 50% style="border:none;">
+            <h1>Providence Anesthesiology Associates, PA </h1>
+            <h2>PAAPA Pay & Assignment Mangagement System</h2>
+            </td>
+        </tr>
+    </table>
+    <hr width="100%" size=10 color=#77B6E2><br><br>';    
+?>
---
+++ includes/logoutDirect.php
@@ -0,0 +1,15 @@
+<?php
+
+echo ' <form name="input" action="logout.php" method="post">
+            <table align="center" width=100%>
+                <tr>
+                    <td align="center">
+                    <input type="submit" name="submit" class="btn" 
+                    value="Logout">
+                </td>
+                </tr>
+            </table>
+            <br>
+            <br>
+            </form>';
+?>
